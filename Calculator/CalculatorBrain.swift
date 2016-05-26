77590a3a972597ec4483fed3fdffe43fdafc9070
Justine Gartner <GartnerJustine@gmail.com>
2016-05-26 18:29:36 -0400

    Added ini for CalculatorBrain

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -11,14 +11,32 @@ import Foundation
 class CalculatorBrain
 {
 
-    enum Op {
+    private enum Op {
         case Operand(Double)
-        case UrnaryOperation(String, Double -> Double)
+        case UnaryOperation(String, Double -> Double)
         case BinaryOperation(String, (Double, Double) -> Double)
     }
     
-    var opStack = [Op]()
+    private var opStack = [Op]()
     
+    private var knownOps = [String:Op]()
     
+    init(){
+        knownOps["×"] = Op.BinaryOperation("×", *)
+        knownOps["÷"] = Op.BinaryOperation("÷"){ $1 / $0 }
+        knownOps["+"] = Op.BinaryOperation("+", +)
+        knownOps["−"] = Op.BinaryOperation("−"){ $1 - $0 }
+        knownOps["√"] = Op.UnaryOperation("√", sqrt)
+    }
+    
+    func pushOperand(operand:Double) {
+        opStack.append(Op.Operand(operand))
+    }
+    
+    func performOperation(symbol: String) {
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+    }
     
 }
